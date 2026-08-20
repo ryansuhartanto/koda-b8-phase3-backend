7f37ade777e26baa89cf5d7e7e97afd0f9e47bbe
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-08-20 16:49:28 +0700

    feat: check for POSTGRES_URL

--- src/models/options.js
+++ src/models/options.js
@@ -1,6 +1,9 @@
 import { PostgresDialect } from "@sequelize/postgres";
 
 /** @type {import("@sequelize/core").Options<PostgresDialect>} */
-const options = { dialect: PostgresDialect };
+const options = {
+	dialect: PostgresDialect,
+	url: process.env["POSTGRES_URL"],
+};
 
 export default options;
